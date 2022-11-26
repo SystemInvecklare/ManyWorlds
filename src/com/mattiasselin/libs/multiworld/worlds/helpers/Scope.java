65d3c1f7a3d25fa905a6b084648dec2645c9e17a
Mattias Selin <mattias.selin@gmail.com>
2022-11-26 12:20:18 +0100

    Add listVar

--- src/com/mattiasselin/libs/multiworld/worlds/helpers/Scope.java
+++ src/com/mattiasselin/libs/multiworld/worlds/helpers/Scope.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mattiasselin.libs.multiworld.expression.Constant;
+import com.mattiasselin.libs.multiworld.expression.Expression;
 import com.mattiasselin.libs.multiworld.expression.IStochasticExpression;
 import com.mattiasselin.libs.multiworld.expression.Variable;
 import com.mattiasselin.libs.multiworld.worlds.IWorlds;
@@ -46,6 +47,38 @@ public class Scope {
 	
 	public <T> Variable<T> var(Class<T> type, String name, T initialValue) {
 		return var(type, name, new Constant<T>(initialValue));
+	}
+	
+	public <T> Variable<List<T>> listVar(Class<T> entryType) {
+		Variable<List<T>> variable = new Variable<>();
+		variables.add(variable);
+		return variable;
+	}
+	
+	public <T> Variable<List<T>> listVar(Class<T> entryType, String name) {
+		Variable<List<T>> variable = new Variable<>(name);
+		variables.add(variable);
+		return variable;
+	}
+	
+	public <T> Variable<List<T>> listVar(Class<T> entryType, IStochasticExpression<? extends List<T>> initialValue) {
+		Variable<List<T>> variable = listVar(entryType);
+		worlds.set(variable, Expression.cast(initialValue));
+		return variable;
+	}
+	
+	public <T> Variable<List<T>> listVar(Class<T> entryType, List<T> initialValue) {
+		return listVar(entryType, new Constant<>(initialValue));
+	}
+	
+	public <T> Variable<List<T>> listVar(Class<T> entryType, String name, IStochasticExpression<? extends List<T>> initialValue) {
+		Variable<List<T>> variable = listVar(entryType, name);
+		worlds.set(variable, Expression.cast(initialValue));
+		return variable;
+	}
+	
+	public <T> Variable<List<T>> listVar(Class<T> entryType, String name, List<T> initialValue) {
+		return listVar(entryType, name, new Constant<>(initialValue));
 	}
 	
 	public void forgetScope() {
